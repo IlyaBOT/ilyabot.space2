20c3ac550fe8eb05bde6ff3ca982e13682dd7088
IlyaBOT <isarbeu@gmail.com>
2024-11-05 14:18:48 +0300

    Add jsFormula

---
+++ scripts/jsFormula.js
@@ -0,0 +1,14 @@
+let freq = 0.0;
+let Pi = 3.14159265358979323846;
+let resistance = 0.0;
+let capacitance = 0.0;
+let inductance = 0.0;
+
+function calculateLowfilter() {
+    freq = document.getElementById("freq").value;
+    resistance = document.getElementById("resistance").value;
+    capacitance = document.getElementById("capacitance").value;
+    inductance = document.getElementById("inductance").value;
+    
+    
+}
